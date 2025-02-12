d58701c906aa6b5d7d656ea272be7d9279107901
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2025-02-12 14:00:50 +0600

    condition js and bmi js

---
+++ condition1.js
@@ -0,0 +1,18 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+
+
+const amount = 500;
+const Burger = 500;
+
+if (amount >= Burger){
+    console.log('A free coke for you sir')
+}
+else{
+    console.log("sir you need to purchase a coke with 30TK")
+}
