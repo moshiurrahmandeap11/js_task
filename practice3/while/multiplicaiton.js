10bebbd1ed0486fa9f0af4ee24a617c592645cdd
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2025-02-13 02:02:35 +0600

    add multiplication js

---
+++ practice3/while/multiplicaiton.js
@@ -0,0 +1,18 @@
+/***
+
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+ */
+
+
+/*programming hero*/
+
+
+let number = 5;
+let i = 1;
+while(i <= 10){
+    let result = number * i;
+    let multiplication = number + " x " + i + " = " + result;
+    console.log(multiplication)
+    i++
+}
