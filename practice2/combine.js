f85d0011697f004b7a85d3666c2a2af1aeccd2bb
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2025-02-12 16:12:55 +0600

    add combine js

---
+++ practice2/combine.js
@@ -0,0 +1,16 @@
+// Create two arrays of your choice.
+
+fruits = ["apple", "cherry", "dragon"]
+sports = ["cricket", "soccer", "wwe"]
+
+
+// Use the concat method to combine the two arrays into a new array.
+let concatArray = fruits.concat(sports)
+// console.log(concatArray)
+
+
+// Print both the original arrays and the combined array using console.log().
+
+console.log(fruits)
+console.log(sports)
+console.log(concatArray)
