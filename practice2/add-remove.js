4e56ce5041fcf3cb04d77d847fa49bb7a3481e0c
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2025-02-12 15:45:11 +0600

    add add-remove js

---
+++ practice2/add-remove.js
@@ -0,0 +1,18 @@
+
+// Declare an array of 3 tourist destinations
+let spot = ["swizerland", "maldives", "sant martin"]
+// console.log(spot)
+// Add a new tourist destination to your tourist array
+spot.push("kashmir")
+// console.log(spot)
+
+// Add two more to your array
+spot.push("australia", "finland")
+// console.log(spot)
+
+// Remove the last tourist destination you have added
+spot.pop()
+// console.log(spot)
+
+// display the final array as output
+console.log(spot)
