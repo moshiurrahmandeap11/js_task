c9ddc405758f425b6ec20f98e29d3bb7f0b13659
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2025-02-13 01:49:41 +0600

    add odd_even

---
+++ practice3/while/odd_even.js
@@ -0,0 +1,41 @@
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+
+/*programming hero*/
+
+
+// Find all the odd numbers from 61 to 100.
+
+
+// let numbers = 61;
+// while(numbers <= 100){
+//     if (numbers % 2 !== 0){
+//         console.log(numbers)
+//     }
+//     numbers++;
+// }
+
+
+// Find all the even numbers from 78 to 98.
+
+let numbers = 78;
+while(numbers <= 98){
+    if (numbers % 2 === 0){
+        console.log(numbers)
+    }
+    numbers++;
+}
